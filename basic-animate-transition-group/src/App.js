81e7a97f23de376aa49ff2db05cdf652f1211cea
webfrontand <fbrud431@gmail.com>
2017-03-15 21:00:27 +0900

    animation

--- basic-animate-transition-group/src/App.js
+++ basic-animate-transition-group/src/App.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      items:['I\'m number 1', 'I\'m number 2', 'I\'m number 3'],
+      itemNumber: 3
+    };
+  }
+
+  onAddItem(){
+    this.setState({
+      itemNumber: this.state.itemNumber + 1,
+      items: this.state.items.concat(['I\'m number '+(this.state.itemNumber + 1)])
+    });
+  }
+
+  onDeleteItem(id){
+    const newItems = this.state.items.slice();
+    newItems.splice(id, 1);
+    this.setState({
+      items: newItems
+    });
+  }
+
   render() {
     return (
       <div>
-        hello
+        <h1 onClick={this.onAddItem.bind(this)}>ADD ITEM</h1>
+        <ul>
+          {this.state.items.map((item, i) => {
+            return (
+              <li key={item} onClick={this.onDeleteItem.bind(this, i)}>
+                {item}
+              </li>
+            )
+          })}
+
+        </ul>
       </div>
     );
   }

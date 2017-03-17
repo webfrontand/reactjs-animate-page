f04fdc83f14c238c20e66d6b83501721e825ce71
webfrontand <fbrud431@gmail.com>
2017-03-17 21:27:42 +0900

    react-router and express

--- velopert-blog-react-router3/src/index.js
+++ velopert-blog-react-router3/src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+import Home from './containers/Home';
+import About from './containers/About';
+import Posts from './containers/Posts';
+import Post from './containers/Post';
+
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 ReactDOM.render(
-  <App />,
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Home} />
+      <Route path="about" component={About} />
+      <Route path="post" component={Posts}>
+        <Route path=":id" component={Post} />
+      </Route>
+    </Route>
+  </Router>,
   document.getElementById('root')
 );
